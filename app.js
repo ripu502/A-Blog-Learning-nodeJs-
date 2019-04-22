8e10305bf3f57e59c7e5579a5b431829119655f8
ripu502 <ripu502@gmail.com>
2019-04-22 10:05:29 +0530

    very basic layout

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use('/', (req, res, next) => {
-    res.send('Hello from blog');
+    res.render('home');
 });
 
 app.listen(3000);
