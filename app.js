85464f8e24b38c3e0122046a5b51ee2620e4cebc
ripu502 <ripu502@gmail.com>
2019-06-07 13:39:10 +0530

    make it better

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
+// const keys = require('./config/keys');
+
+const helmet = require('helmet');
+const compression = require('compression');
+
+const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(helmet());
+app.use(compression());
 
 const router = require('./routes/router');
 
+
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(router);
 
-mongoose.connect(keys.mongoURI,  { useNewUrlParser: true }).then((result)=>{
+mongoose.connect(`mongodb+srv://${process.env.MongoUser}:${process.env.MongoPassword}@cluster0-jywn3.mongodb.net/${process.env.MongoDataBase}?retryWrites=true`, { useNewUrlParser: true }).then((result) => {
     console.log('connected');
-    app.listen(3000);
-}).catch((err)=>{
+    app.listen(port);
+}).catch((err) => {
     console.log(err);
 });
